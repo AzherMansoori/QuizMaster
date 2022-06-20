d1c3254337a0f16fda7a9178b1a42c26437985a2
Mansoori <azzrudin.mansoori@capgemini.com>
2022-06-20 20:00:42 +0530

    google login added

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import {
     Text,
     View,
@@ -6,23 +6,32 @@ import {
     TextInput,
     TouchableOpacity,
     ImageBackground,
-    Image
+    Image,
+    Alert
 } from 'react-native';
 import * as Color from '../res/Color'
 import AsyncStorage from '@react-native-community/async-storage'
 import AppContext from "../context/AppContext";
-import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+GoogleSignin.configure({
+    webClientId: '1025617248546-0bohkobkdnlh5j8vi4kqttdeg3t0egha.apps.googleusercontent.com'
+});
 function Login({ navigation }) {
     const { setLogin } = useContext(AppContext);
 
-
-    const signIn = async () => {
-        setLogin(true)
-        return
+    async function onGoogleButtonPress() {
         try {
             await GoogleSignin.hasPlayServices();
-            const userInfo = await GoogleSignin.signIn();
-            alert(userInfo)
+            await GoogleSignin.revokeAccess();
+            await GoogleSignin.signOut();
+            // Get the users ID token
+            const { idToken } = await GoogleSignin.signIn();
+            // Create a Google credential with the token
+            const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+            // Sign-in the user with the credential
+            const userInfo = await auth().signInWithCredential(googleCredential)
+            Alert.alert("Success", JSON.stringify(userInfo))
         } catch (error) {
             if (error.code === statusCodes.SIGN_IN_CANCELLED) {
                 // user cancelled the login flow
@@ -32,8 +41,56 @@ function Login({ navigation }) {
                 console.log('IN_PROGRESS');
             } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
                 // play services not available or outdated
-                alert('Google Play Services Unavailable')
             } else {
+                console.log(JSON.stringify(error));
+                // some other error happened
+            }
+        }
+
+
+    }
+
+    const signIn = async () => {
+        // setLogin(true)
+        try {
+            await GoogleSignin.hasPlayServices();
+            await GoogleSignin.revokeAccess();
+            await GoogleSignin.signOut();
+            const isSignedIn = await GoogleSignin.isSignedIn();
+
+            debugger
+            if (isSignedIn) {
+                const currentUser = await GoogleSignin.getCurrentUser();
+            } else {
+                debugger
+                const { idToken, accessToken } = await GoogleSignin.signIn();
+            }
+            debugger
+
+
+            // Create a Google credential with the token
+
+
+
+
+
+            //console.log('userInfo', userInfo);
+            Alert.alert("Success", JSON.stringify(userInfo))
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                Alert.alert('SIGN_IN_CANCELLED', error.code)
+                // user cancelled the login flow
+                console.log('SIGN_IN_CANCELLED');
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                // operation (f.e. sign in) is in progress already
+                Alert.alert("IN_PROGRESS", error.code)
+                console.log('IN_PROGRESS');
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                // play services not available or outdated
+                Alert.alert('Google Play Services Unavailable')
+            } else {
+                console.log(JSON.stringify(error));
+                Alert.alert("Else", JSON.stringify(error))
                 // some other error happened
             }
         }
@@ -50,7 +107,7 @@ function Login({ navigation }) {
                     style={{ width: 230, height: 55 }}
                     size={GoogleSigninButton.Size.Wide}
                     color={GoogleSigninButton.Color.Dark}
-                    onPress={() => signIn()}
+                    onPress={() => onGoogleButtonPress()}
                 // disabled={this.state.isSigninInProgress} 
                 />
             </View>
